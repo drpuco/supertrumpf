e02eee2b4723aba0c9cf7240c1d0c69314675057
Honk2 <honkderzweite@gmail.com>
2023-04-14 09:15:39 +0200

    Init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Card from './Card';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Supertrumpf</h1>
+        <Card />
+        <Card />
+        <Card />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
