d93d582bb19195fb1ddc2cba3d00d353b7c97f42
Honk2 <honkderzweite@gmail.com>
2023-04-14 09:40:50 +0200

    Erweiterung von Cards mit der Animal-Klasse

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,18 +1,26 @@
 import * as React from 'react';
 import './Card.css';
+import Animal from './Animal';
 
 export default function Card(){
+    const elephant = new Animal('Elefant', 'placeholder.png', 3.3, 6000, 70, 1, 40);
     return (
         <div className='card'>
-            <h1>Elefant</h1>
-            <img alt='Elefant' height='200' width='200'  src={`${process.env.PUBLIC_URL}/placeholder.png`} />
+            <h1>{elephant.name}</h1>
+            <img alt={elephant.image} height='200' width='200'  src={`${process.env.PUBLIC_URL}/placeholder.png`} />
             <table>
                 <tbody>
-                    <tr><td>Größe</td><td>3.3m</td></tr>
-                    <tr><td>Gewicht</td><td>6000kg</td></tr>
-                    <tr><td>Alter</td><td>70 Jahre</td></tr>
-                    <tr><td>Nachkommen</td><td>1</td></tr>
-                    <tr><td>Geschwindigkeit</td><td>40km/h</td></tr>
+                    {Object.keys(Animal.properties).map(property =>{
+                        const animalProperty = Animal.properties[property];
+                        return (
+                            <tr key={property}>
+                                <td>{animalProperty.label}</td>
+                                <td>{elephant[property]}&nbsp;
+                                    {animalProperty.unit}
+                                </td>
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
         </div>
