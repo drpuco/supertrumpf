f36612dc0441fb89c7b87ab1bf6685f34e236445
Honk2 <honkderzweite@gmail.com>
2023-04-19 10:44:58 +0200

    Gamejsx hinzugefügt

---
+++ src/Game.jsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+
+import ',/Game.css';
+import Card from './Card';
+import Animal from './Animal';
+
+export default class Game extends React.Component{
+    render(){
+        const playersTurn = true;
+        const player = [
+            new Animal('Elefant','placeholder.png', 3.3,6000,70,1,40),
+        ];
+        const computer = [
+            new Animal('Nashorn', 'placeholder.png', 3.2,2300, 50, 1, 50)
+        ];
+        return(
+            <div>
+                <div className='info'>
+                    {playersTurn ? 'Du bist' : 'Der Computer ist'} an der Reihe
+                </div>
+                <div className='cards'>
+                    <Card animal={player[0]} uncovered={playersTurn} />
+                    <Card animal={computer[0]} uncovered={!playersTurn} />
+                </div>
+            </div>
+        );
+    }
+}
